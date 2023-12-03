c7b394fd2fe8f6d2499ee7fbefcc1d02121c8eb0
a-krasilskaya <r02062008@gmail.com>
2023-12-03 21:37:49 +0300

    fix debug

--- dentalstore/dentalstore/settings.py
+++ dentalstore/dentalstore/settings.py
@@ -27,7 +27,7 @@ CART_SESSION_ID = 'cart'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
 ALLOWED_HOSTS = ["*"]
 
